f892613b07b8a7d98ce4994fd4ee5d3d8a76891d
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-02 17:27:49 +0300

    Update page.tsx

--- app/project/[id]/page.tsx
+++ app/project/[id]/page.tsx
@@ -1,15 +1,12 @@
 import Image from 'next/image';
 import Link from 'next/link';
-
 import { getCurrentUser } from '@/lib/session';
 import { getProjectDetails } from '@/lib/actions';
 import Modal from '@/components/Modal';
-// import ProjectActions from "@/components/ProjectActions"
-// import RelatedProjects from '@/components/RelatedProjects';
 import { ProjectInterface } from '@/common.types';
 import RelatedProjects from '@/components/RelatedProjects';
 import ProjectActions from '@/components/ProjectActions';
-// import ProjectActions from '@/components/ProjectActions';
+
 
 const Project = async ({ params: { id } }: { params: { id: string } }) => {
   const session = await getCurrentUser();
