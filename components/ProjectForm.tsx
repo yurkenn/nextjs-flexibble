ca2382b071527e5a3edc6568c3ed3459d78acb15
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-02 17:09:14 +0300

    Update ProjectForm.tsx

--- components/ProjectForm.tsx
+++ components/ProjectForm.tsx
@@ -1,28 +1,29 @@
 'use client';
-import { SessionInterface } from '@/common.types';
+import { ProjectInterface, SessionInterface } from '@/common.types';
 import Image from 'next/image';
 import React, { ChangeEvent, useState } from 'react';
 import FormField from './FormField';
 import { categoryFilters } from '@/constants';
 import CustomMenu from './CustomMenu';
 import Button from './Button';
-import { createNewProject, fetchToken } from '@/lib/actions';
+import { createNewProject, fetchToken, updateProject } from '@/lib/actions';
 import { useRouter } from 'next/navigation';
 
 type Props = {
   type: string;
   session: SessionInterface;
+  project?: ProjectInterface;
 };
 
-const ProjectForm = ({ type, session }: Props) => {
+const ProjectForm = ({ type, session, project }: Props) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [form, setForm] = useState({
-    title: '',
-    description: '',
-    image: '',
-    liveSiteUrl: '',
-    githubUrl: '',
-    category: '',
+    title: project?.title || '',
+    description: project?.description || '',
+    image: project?.image || '',
+    liveSiteUrl: project?.liveSiteUrl || '',
+    githubUrl: project?.githubUrl || '',
+    category: project?.category || '',
   });
 
   const router = useRouter();
@@ -37,6 +38,10 @@ const ProjectForm = ({ type, session }: Props) => {
       if (type === 'create') {
         // create project
         await createNewProject(form, session?.user?.id, token);
+        router.push('/');
+      }
+      if (type === 'edit') {
+        await updateProject(form, project?.id as string, token);
         router.push('/');
       }
     } catch (error) {
@@ -128,7 +133,7 @@ const ProjectForm = ({ type, session }: Props) => {
           title={
             isSubmitting
               ? `${type === 'create' ? 'Creating' : 'Editing'}`
-              : `${type === 'create' ? 'Create' : 'Edit'} Project`
+              : `${type === 'create' ? 'Create' : 'Edit'}`
           }
           type="submit"
           leftIcon={isSubmitting ? '' : '/plus.svg'}
