c4fa8f47bd9a6c92ae7f2b3f4dbe9de6d07c7a41
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-02 17:52:55 +0300

    Update Share Work add button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import AuthProviders from './AuthProviders';
 import { getCurrentUser } from '@/lib/session';
 import ProfileMenu from './ProfileMenu';
+import Button from './Button';
 
 const Navbar = async () => {
   const session = await getCurrentUser();
@@ -25,7 +26,9 @@ const Navbar = async () => {
         {session?.user ? (
           <>
             <ProfileMenu session={session} />
-            <Link href="/create-project">Share Work</Link>
+            <Link href="/create-project">
+              <Button type="button" title="Share work" />
+            </Link>
           </>
         ) : (
           <AuthProviders />
