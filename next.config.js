83fd5cdf377b74b1938e3c3cf75cb64c3362c4d1
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-02 17:09:22 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ['lh3.googleusercontent.com', 'res.cloudinary.com'],
   },
+
   experimental: {
     serverComponentsExternalPackages: ['cloudinary', 'graphql-request'],
   },
