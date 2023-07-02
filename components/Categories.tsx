5dd9606b1252e451b1b601416c644cf96564a56d
oğuz yürken <69719126+yurkenn@users.noreply.github.com>
2023-07-02 17:09:03 +0300

    Create Categories.tsx

---
+++ components/Categories.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { categoryFilters } from '@/constants';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+
+const Categories = () => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const category = searchParams.get('category') || '';
+
+  const handleTags = (filter: string) => {
+    router.push(`${pathname}?category=${filter}`);
+  };
+
+  return (
+    <div className="flexBetween w-full gap-5 flex-wrap">
+      <ul className="flex gap-2 overflow-auto">
+        {categoryFilters.map((filter) => (
+          <button
+            type="button"
+            key={filter}
+            onClick={() => handleTags(filter)}
+            className={`
+          ${category === filter ? 'bg-light-white-300 font-medium' : 'font-normal'}
+          px-4 py-3 rounded-lg capitalize whitespace-nowrap
+          `}
+          >
+            {filter}
+          </button>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
